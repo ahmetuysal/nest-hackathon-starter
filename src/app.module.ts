c67a117024d1ce9c92483cb680526d86dd4c4437
Ahmet Uysal <auysal16@ku.edu.tr>
2019-08-30 15:22:26 +0300

    Added typeorm and pg, created ormconfig file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
