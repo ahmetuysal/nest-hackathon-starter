1aa5b9a1e35718a345bc71bce2184ee7ead8a805
Ahmet Uysal <auysal16@ku.edu.tr>
2019-08-30 23:45:49 +0300

    Removed empty app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -8,7 +6,5 @@ import { MailSenderModule } from './mail-sender/mail-sender.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, MailSenderModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
