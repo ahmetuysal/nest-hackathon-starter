67580dc10921f60d27f84d35ff0798d41f0d26c6
Ahmet Uysal <auysal16@ku.edu.tr>
2019-09-04 22:18:16 +0300

    Added helmet global middleware

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(helmet());
 
   const options = new DocumentBuilder()
     .setTitle('NestJS Hackathon Starter by @ahmetuysal')
