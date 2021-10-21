62c2e8d81eeb43a2f77eeac687198fca09d49f91
Ahmet Uysal <auysal16@ku.edu.tr>
2021-10-21 16:38:22 +0300

    feat: add prisma

---
+++ src/common/services/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: ['error', 'warn'],
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
